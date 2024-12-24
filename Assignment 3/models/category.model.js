abbda3c71a06fb6d4867b697ede05af2811e4c77
Sufyan Liaqat <155754649+sufyan2618@users.noreply.github.com>
2024-12-24 16:49:25 +0500

    Add files via upload
    Assignment 3

---
+++ Assignment 3/models/category.model.js
@@ -0,0 +1,24 @@
+// models/category.js
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Category', categorySchema);
